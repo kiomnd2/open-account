64fb0d3a7edcf2a5263d6847b486f674e71d141c
kiomnd2 <kiomnd2@naver.com>
2020-08-09 03:27:36 +0900

    이력조회시, 조건을 변경

--- src/main/java/com/kakao/openaccount/repository/TransferHistoryRepository.java
+++ src/main/java/com/kakao/openaccount/repository/TransferHistoryRepository.java
@@ -4,5 +4,5 @@ import com.kakao.openaccount.domain.TransferHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransferHistoryRepository extends JpaRepository<TransferHistory, Long> {
-    TransferHistory findByUserUUIDAndTransferUUID(String userUUID, String transferUUID);
+    TransferHistory findByUserUUID(String userUUID);
 }
